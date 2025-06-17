a3a7489055864e32a5ad457194bf5309e49527d2
Garrison Snelling <garrison@hey.com>
2025-06-17 15:02:55 -0500

    removing ui in favor of zforge

--- pkg/ui/css/doc.go
+++
@@ -1,5 +0,0 @@
-// Package css provides CSS utility functions for the ComputeSDK UI framework.
-// The utilities are generated from YAML configuration files.
-package css
-
-//go:generate go run ./internal/tool
